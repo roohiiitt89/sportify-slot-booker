5f5bec6ada3155ba69385771afc2c34cf591271c
roohiiitt89 <rohitsb567@proton.me>
2025-04-28 19:39:45 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -30,10 +29,10 @@ const App = () => (
             <Route path="/signup" element={<SignUp />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/venues" element={<Venues />} />
+            {/* Add a dynamic route for Venue Details */}
             <Route path="/venues/:id" element={<VenueDetail />} />
             <Route path="/sports" element={<Sports />} />
             <Route path="/sports/:id" element={<SportDetail />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
